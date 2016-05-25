8101868df4d18c3b41ce2966f601fc4cdf90d269
Matthias Pinnewaert <pinnewaert.matthias@gmail.com>
2016-05-25 13:24:44 +0200

    Add files via upload

---
+++ loaded/js/twitchapi.js
@@ -0,0 +1,19 @@
+var id ='g1kobimxlknrwgoouk8h1mz4ribnip';
+var homeLink = 'http://pinny91.github.io/twitchlist/loaded';
+
+var authenURL = 'https://api.twitch.tv/kraken/oauth2/authorize?' +
+				'response_type=token&client_id=' + id +
+				'&redirect_uri=' + homeLink +
+				'&scope=' + 'user_read';
+var accToken = 'Nope';				
+if(accToken === 'Nope'){
+	accToken = document.location.hash;
+}
+
+dataURL = 'https://api.twitch.tv/kraken/users/shulyx' + '?oauth_token=' + accToken + '&callback=?';
+
+
+$.getJSON(dataURL, function(data) {
+  console.log(data);
+
+});
