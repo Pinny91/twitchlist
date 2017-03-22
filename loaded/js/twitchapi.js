3c171581e5bf509bd130270932629e9c82cc6245
Matthias Pinnewaert <pinnewaert.matthias@gmail.com>
2017-03-22 16:10:49 +0100

    Add files via upload

--- loaded/js/twitchapi.js
+++ loaded/js/twitchapi.js
@@ -56,10 +56,10 @@ $.getJSON(dataURL, function(followersListData) {
 				$(listId).addClass('offline');
 			}
 			else {
-				var logoURL = streamData.stream.channel.logo;
-				var channelname = streamData.stream.channel.name;
-				var game = streamData.stream.game;
-				var channelURL = streamData.stream.channel.url;
+				var logoURL = streamData.logo;
+				var channelname = streamData.name;
+				var game = streamData.game;
+				var channelURL = streamData.url;
 				$('#list-streamers ul').append(
 					'<a target="_blank" href="' + channelURL + '">' +
 					'<li id="num' + teller +'"class="btn btn-default">'+ 
