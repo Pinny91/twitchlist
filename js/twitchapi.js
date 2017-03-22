0d02ae6be6f2dd06b74b67507e89650e4a28a691
Matthias Pinnewaert <pinnewaert.matthias@gmail.com>
2017-03-22 14:47:05 +0100

    Update twitchapi.js

--- js/twitchapi.js
+++ js/twitchapi.js
@@ -2,7 +2,7 @@ var id ='g1kobimxlknrwgoouk8h1mz4ribnip';
 var homeLink = 'http://pinny91.github.io/twitchlist/loaded';
 
 var authenURL = 'https://api.twitch.tv/kraken/oauth2/authorize?' +
-				'response_type=token&client_id=' + id +
+				'response_type=code&client_id=' + id +
 				'&redirect_uri=' + homeLink +
 				'&scope=' + 'user_read';
 var accToken = 'Nope';		
